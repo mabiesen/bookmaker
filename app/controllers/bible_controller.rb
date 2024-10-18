8d62d8c8580fc1ce7aaf6eed01205f3e9697900e
Matthew Biesen <mabiesen@outlook.com>
2024-10-18 17:15:43 -0500

    changes

--- app/controllers/bible_controller.rb
+++ app/controllers/bible_controller.rb
@@ -1,8 +1,15 @@
 class BibleController < ApplicationController
   def home; end
 
+  # GET request for json, returns raw bible_verses
   def verses
     @bible_verses = BibleVerse.order(book_number: :asc, chapter: :asc, verse: :asc)
     render json: @bible_verses
   end
+
+  # GET request, returns summary of verse notes, collections, etc.
+  def verse_summary
+    @bible_verse = BibleVerse.find(params[:id])
+  end
+
 end
