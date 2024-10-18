6762dc933fa32e9a4bdba1ddfca41063ddf7b63e
Matthew Biesen <upandcomming88@gmail.com>
2024-10-18 13:06:05 -0500

    correct verse ordering (#46)

--- app/controllers/bible_controller.rb
+++ app/controllers/bible_controller.rb
@@ -1,5 +1,5 @@
 class BibleController < ApplicationController
   def home
-    @bible_verses = BibleVerse.all
+    @bible_verses = BibleVerse.order(book_number: :asc, chapter: :asc, verse: :asc)
   end
 end
