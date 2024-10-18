83961c5aa63611cf4fb5fe8f4aad5003e85c97ca
Matthew Biesen <upandcomming88@gmail.com>
2024-10-18 14:02:58 -0500

    datatables ajax for bible (#47)
    * datatables ajax for bible

--- app/controllers/bible_controller.rb
+++ app/controllers/bible_controller.rb
@@ -1,5 +1,8 @@
 class BibleController < ApplicationController
-  def home
+  def home; end
+
+  def verses
     @bible_verses = BibleVerse.order(book_number: :asc, chapter: :asc, verse: :asc)
+    render json: @bible_verses
   end
 end
